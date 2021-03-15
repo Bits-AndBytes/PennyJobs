7f3cab10f26c3722430de0f2222b7fba68fe1725
adhitamapatrick <adhitama@sheridancollege.ca>
2021-03-15 15:29:26 -0400

    Create a new interface called JobRepository and update GetMapping and PostMapping for addJob method

--- src/main/java/ca/sheridancollege/pennyjobs/controllers/JobController.java
+++ src/main/java/ca/sheridancollege/pennyjobs/controllers/JobController.java
@@ -1,10 +1,20 @@
 package ca.sheridancollege.pennyjobs.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import ca.sheridancollege.pennyjobs.beans.Job;
+import ca.sheridancollege.pennyjobs.repositories.JobRepository;
 
 @Controller
 public class JobController {
+	
+	@Autowired
+	private JobRepository jRepo;
 	
 	@GetMapping("/")
 	public String loadRoot() {
@@ -21,9 +31,18 @@ public class JobController {
 		return "";
 	}
 	
-	@GetMapping("jobposting")
-	public String loadJobPosting() {
+	@GetMapping("jobpost")
+	public String loadAddJob(Model model, @ModelAttribute Job job) {
+		model.addAttribute("job", new Job());
 		return "JobForm.html";
+	}
+	
+	@PostMapping("jobpost")
+	public String addJob(Model model, @ModelAttribute Job job) {
+		jRepo.save(job);
+		model.addAttribute("job", new Job());
+		model.addAttribute("jobs", jRepo.findAll());
+		return "JobList.html";
 	}
 	
 	@GetMapping("joblist")
