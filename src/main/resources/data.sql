dd728a36d9f4a4b773c51e50260bfde583063bf0
WeiyeChen24 <68452004+WeiyeChen24@users.noreply.github.com>
2021-04-01 01:48:52 -0400

    Changes
    change the values inside Account.java and add a new Role.java. Add two
    more repositroies(Account, Role). Add LoginAccessDeniedHandler,
    UserDetailsServiceImpl and change the SecurityCheck. Add data.sql to
    access to the H2 and add 4 Role

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+
+insert into Role (rolename)
+values ('ROLE_STUDENT');
+ 
+insert into Role (rolename)
+values ('ROLE_PARENT');
+
+insert into Role (rolename)
+values ('ROLE_POSTER');
+
+insert into Role (rolename)
+values ('ROLE_ADMIN');
+
