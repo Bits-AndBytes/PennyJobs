5476ca862f9c40db530dfe87b515fb849bc262af
GregKnott <46542835+GregKnott@users.noreply.github.com>
2021-04-08 13:57:08 -0400

    Small changes
    Removed unnecessary html pages, implemented browse jobs page with search
    function. Changed code to correctly link new accounts with an account
    id.

--- src/main/java/ca/sheridancollege/pennyjobs/controllers/JobController.java
+++ src/main/java/ca/sheridancollege/pennyjobs/controllers/JobController.java
@@ -1,5 +1,8 @@
 package ca.sheridancollege.pennyjobs.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -7,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ca.sheridancollege.pennyjobs.beans.Account;
 import ca.sheridancollege.pennyjobs.beans.Job;
@@ -64,11 +68,6 @@ public class JobController {
 		return "JobForm.html";
 	}
 	
-	@GetMapping("/joblist")
-	public String loadJobList() {
-		return "viewjobs.html";
-	}
-	
 	@GetMapping("/poster")
 	public String loadPoster(Model model, Authentication auth){
 		
@@ -89,5 +88,32 @@ public class JobController {
 		}
 		
 		return "poster.html";
+	}
+	
+	
+	@GetMapping("/jobs")
+	public String leadSearch(Model model) {
+		
+		model.addAttribute("jobs", jRepo.findAll());
+		
+		return "jobs.html";
+	}
+	
+	@GetMapping("/jobs/search")
+	public String searchDB(Model model, @RequestParam String searchType, @RequestParam String query) {
+		
+		List<Job> results = new ArrayList<Job>();
+		
+		switch (searchType){
+		case "Title":
+			results = jRepo.findAllByTitleIgnoreCaseContains(query);
+			break;
+		case "Description":
+			results = jRepo.findAllByDescriptionIgnoreCaseContains(query);
+			break;
+		}
+		model.addAttribute("jobs", results);
+		
+		return "jobs.html";
 	}
 }
