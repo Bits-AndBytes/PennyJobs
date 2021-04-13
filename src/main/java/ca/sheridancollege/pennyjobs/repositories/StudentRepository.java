4e74a393bb5a49b4697386f2305ba6ea8f6398d6
GregKnott <46542835+GregKnott@users.noreply.github.com>
2021-04-13 16:40:35 -0400

    Added ability to have students assign themselves to a job

--- src/main/java/ca/sheridancollege/pennyjobs/repositories/StudentRepository.java
+++ src/main/java/ca/sheridancollege/pennyjobs/repositories/StudentRepository.java
@@ -2,8 +2,11 @@ package ca.sheridancollege.pennyjobs.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 
+import ca.sheridancollege.pennyjobs.beans.Account;
 import ca.sheridancollege.pennyjobs.beans.Student;
 
 public interface StudentRepository extends CrudRepository<Student, Integer> {
+	
+	public Student findByAccount(Account account);
 
 }
